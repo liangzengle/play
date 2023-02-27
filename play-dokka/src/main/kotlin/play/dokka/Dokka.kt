02e0da67337d282794d255535e1843a3ec3c3225
LiangZengle <liangzengle@gmail.com>
2023-02-27 17:30:46 +0800

    Refactor Dokka.generate

--- play-dokka/src/main/kotlin/play/dokka/Dokka.kt
+++ play-dokka/src/main/kotlin/play/dokka/Dokka.kt
@@ -1,10 +1,10 @@
 package play.dokka
 
-import org.jetbrains.dokka.DokkaConfigurationImpl
 import org.jetbrains.dokka.base.testApi.testRunner.BaseAbstractTest
 import org.jetbrains.dokka.model.DModule
 import org.jetbrains.dokka.plugability.DokkaPlugin
 import play.dokka.model.ClassDescriptorList
+import testApi.testRunner.TestDokkaConfigurationBuilder
 import java.io.File
 
 /**
@@ -13,32 +13,59 @@ import java.io.File
  */
 object Dokka {
 
-  fun generate(projectDir: File): ClassDescriptorList {
-    return generate(projectDir, { it }, DefaultTransformer)
+  const val MAIN_JAVA = "src/main/java"
+
+  const val MAIN_KOTLIN = "src/main/kotlin"
+
+  /**
+   * What is a projectDir: {projectDir}/src/main/kotlin
+   *
+   * @param projectDir File
+   * @return ClassDescriptorList
+   */
+  fun generate(vararg projectDir: File): ClassDescriptorList {
+    return generate(listOf(*projectDir))
+  }
+
+  fun generate(projectDirs: List<File>): ClassDescriptorList {
+    return generate(projectDirs) { }
+  }
+
+  fun generate(
+    projectDirs: List<File>,
+    configurationCustomizer: TestDokkaConfigurationBuilder.() -> Unit
+  ): ClassDescriptorList {
+    return generate(projectDirs, configurationCustomizer, DefaultTransformer)
   }
 
   fun <T> generate(
-    projectDir: File,
-    configurationOverride: (DokkaConfigurationImpl) -> DokkaConfigurationImpl,
+    projectDirs: List<File>,
+    configurationCustomizer: TestDokkaConfigurationBuilder.() -> Unit,
     transformer: (List<DModule>) -> T
   ): T {
-    return Generator.generate(projectDir, configurationOverride, transformer)
+    return Generator.generate(projectDirs, configurationCustomizer, transformer)
+  }
+
+  fun defaultSourceRoots(projectDir: File): Sequence<String> {
+    return sequenceOf(projectDir.resolve(MAIN_KOTLIN).absolutePath, projectDir.resolve(MAIN_JAVA).absolutePath)
+  }
+
+  fun TestDokkaConfigurationBuilder.withDefaultSourceSet(projectDirs: List<File>) {
+    sourceSet {
+      sourceRoots = projectDirs.asSequence().flatMap(::defaultSourceRoots).toList()
+    }
   }
 
   private object Generator : BaseAbstractTest() {
     fun <T> generate(
-      projectDir: File,
-      configurationOverride: (DokkaConfigurationImpl) -> DokkaConfigurationImpl,
+      projectDirs: List<File>,
+      configurationCustomizer: TestDokkaConfigurationBuilder.() -> Unit,
       transformer: (List<DModule>) -> T
     ): T {
-      val cfg = configurationOverride(
-        dokkaConfiguration {
-          sourceSet {
-            sourceRoots = listOf("src/main/kotlin", "src/main/java").map { projectDir.resolve(it).absolutePath }
-          }
-        }
-      )
-
+      val cfg = dokkaConfiguration {
+        withDefaultSourceSet(projectDirs)
+        configurationCustomizer()
+      }
       val holder = arrayOfNulls<Any>(1)
       testFromData(cfg, pluginOverrides = listOf(object : DokkaPlugin() {})) {
         preMergeDocumentablesTransformationStage = { models ->
